5b8eeb0794e9de09008a58985b8ce61cbad2bd8b
Shubham Gaur <54438256+GaurSay@users.noreply.github.com>
2021-12-29 10:04:57 +0530

    updated with 2 methods

--- find-the-duplicate-number/find-the-duplicate-number.java
+++ find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,16 +1,33 @@
 class Solution {
     public int findDuplicate(int[] arr) {
+        int slow = arr[0];
+        int fast = arr[0];
         
-        for(int i=0;i<arr.length;i++){
-            int index = Math.abs(arr[i]);
-            int value = arr[index];
-            if(value > 0){
-                arr[index] = value*(-1);
-            }
-            else{
-                return Math.abs(arr[i]);
-            }
+        do{
+            slow = arr[slow];
+            fast= arr[arr[fast]];
+        }while(slow!=fast);
+            
+        fast = arr[0];
+        
+        while(slow!=fast){
+            slow = arr[slow];
+            fast = arr[fast];
         }
-        return -1;
+        return slow;
+        
+        // SWITCH METHOD
+        
+        // for(int i=0;i<arr.length;i++){
+        //     int index = Math.abs(arr[i]);
+        //     int value = arr[index];
+        //     if(value > 0){
+        //         arr[index] = value*(-1);
+        //     }
+        //     else{
+        //         return Math.abs(arr[i]);
+        //     }
+        // }
+        // return -1;
     }
 }
