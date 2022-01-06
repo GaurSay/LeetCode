423db609964f67c8ddc9f0424062ab8353422cf2
Shubham Gaur <54438256+GaurSay@users.noreply.github.com>
2022-01-06 13:00:25 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (7.57%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
@@ -11,6 +11,7 @@ class Solution {
         if(node == null){
             return;
         }
+        //copy val to this node and point it to next.next
         node.val = node.next.val;
         node.next = node.next.next;
         
