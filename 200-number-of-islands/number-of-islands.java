a7c06f368fd440ddeae4da7a83b1c13296d4b867
Shubham Gaur <54438256+GaurSay@users.noreply.github.com>
2024-12-20 08:43:44 +0530

    Time: 6 ms (25.02%) | Memory: 53.2 MB (5.67%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,29 +2,58 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int visited[][] = new int[m][n];
-        int islands=0;
+        int visited[][]= new int[m][n];
+        Queue<Pair> q = new LinkedList<>();
+
+        int islands = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j] == '1' && visited[i][j] == 0){
-                    islands++;
-                    dfs(grid,i,j);
+                    islands ++;
+                    bfs(grid,visited,i,j,m,n);
                 }
             }
         }
         return islands;
-        
+
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
-            return;
+    public void bfs(char[][] grid,int[][] vis,int i,int j,int m,int n) {
+        vis[i][j] = 1;
+
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,j));
+
+        while(!q.isEmpty())
+        {
+            Pair curr = q.poll();
+
+           // delta row and delta column
+            int delrow[] = {-1, 0, +1, 0}; 
+            int delcol[] = {0, -1, 0, +1}; 
+
+
+            for (int k = 0; k < 4; k++) {
+                int nrow = curr.f + delrow[k];
+                int ncol = curr.s + delcol[k];
+                // check for valid unvisited land neighbour coordinates
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
+                    vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1') {
+                    q.add(new Pair(nrow,ncol));    
+                    vis[nrow][ncol] = 1;
+                }
+            }
         }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+    }
+
+
+}
+
+class Pair{
+    int f;
+    int s ;
+    public Pair(int f,int s){
+        this.f=f;
+        this.s=s;
     }
 }
